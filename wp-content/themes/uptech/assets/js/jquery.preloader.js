9bb03cf15288468c54259690dabe9a23b514840f
Liki Alopex <liki@waterfordpress.co.nz>
2023-07-14 22:32:01 +1200

    Initial commit

---
+++ wp-content/themes/uptech/assets/js/jquery.preloader.js
@@ -0,0 +1,8 @@
+
+(function( $ ){
+	'use strict';
+	$(window).on('load',function(){
+	    $('.st-preloader').css({ 'opacity':'0', 'visibility' : 'hidden'});
+	    $('body').css({'overflow':'auto'})
+	});
+})( jQuery );
